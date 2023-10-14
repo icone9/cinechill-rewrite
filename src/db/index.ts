c319d430d981e83989ca11b8ac3bc8b1a05a7e90
jbob9 <berlenskyoliver99@gmail.com>
2023-10-14 19:51:55 -0400

    use mysql in dev

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,13 +1,29 @@
 import { drizzle } from "drizzle-orm/planetscale-serverless";
 import { connect } from "@planetscale/database";
-import { DATABASE_HOST, DATABASE_USERNAME, DATABASE_PASSWORD } from '$env/static/private';
+import { DATABASE_HOST, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_NAME } from '$env/static/private';
 import * as schema from './schema';
+import mysql from "mysql2/promise";
+import { drizzle as drizzleMysql } from "drizzle-orm/mysql2";
  
-// create the connection
-export const connection = connect({
-  host: DATABASE_HOST,
-  username: DATABASE_USERNAME,
-  password: DATABASE_PASSWORD,
-});
- 
-export const db = drizzle(connection, { schema, logger: true });
+console.log(import.meta.env.PROD, 'import.meta.env.PROD')
+
+function createPlanetScaleConnection() {
+  const planetscaleConnection = connect({
+    host: DATABASE_HOST,
+    username: DATABASE_USERNAME,
+    password: DATABASE_PASSWORD,
+  });
+  return drizzle(planetscaleConnection, { schema: schema, logger: true })
+}
+
+async function createMysqlConnection() {
+  const mysqlConnection = await mysql.createConnection({
+    host: DATABASE_HOST,
+    user: DATABASE_USERNAME,
+    database: DATABASE_NAME,
+    password: DATABASE_PASSWORD
+  });
+  return drizzleMysql(mysqlConnection, { schema: schema, logger: true, mode: 'default' });
+}
+
+export const db = import.meta.env.PROD ? createPlanetScaleConnection() : await createMysqlConnection();
